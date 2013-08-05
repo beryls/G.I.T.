ab21acf43b16ec273a74ba44848e9097fff2dfe7
Theodore Cleveland <tcclevela@gmail.com>
2013-08-05 16:37:04 -0400

    updates authentication to update user info on login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,19 +2,18 @@ class UsersController < ApplicationController
 
 	def callback
   	result = RestClient.post("https://github.com/login/oauth/access_token",
-												    {client_id: ENV['CLIENT_ID'],
-												     client_secret: ENV['CLIENT_SECRET'],
-												     code: params[:code]
-												    },{
-												     :accept => :json
-												    });
+	    {client_id: ENV['CLIENT_ID'],
+	     client_secret: ENV['CLIENT_SECRET'],
+	     code: params[:code]
+	    },{
+	     :accept => :json
+	    });
   	@@access_token = JSON.parse(result)['access_token']
   	redirect_to '/users/create'
 	end
 
 
 	def create
-
 		user = JSON.parse(RestClient.get("https://api.github.com/user", {:params => {:access_token => @@access_token}}))
   	user_info = {name: user['name'], 
   		login: user['login'], 
@@ -24,6 +23,7 @@ class UsersController < ApplicationController
   		repos_count: user['public_repos']}
   	user = User.create_user(user_info)
   	redirect_to "/sessions/#{user.id}/create"
+  	# decide whether or not to add repos at this time
 	end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,14 @@ class User < ActiveRecord::Base
 	attr_accessible :name, :login, :email, :avatar_url, :access_token, :repos_count
 
 	def self.create_user(info)
-		if(User.find_by_email(info['email']))
-			# update user information here
-			return User.find_by_email(info['email'])
-
+		if(User.find_by_email(info[:email]))
+			user = User.find_by_email(info[:email])
+			user.update_attributes(info)
+			return user
 		else
-			
 			user = User.create(info)
 			return user
-			
+
 		end
 	end	
 end
