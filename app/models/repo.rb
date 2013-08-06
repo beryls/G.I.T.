dfec8eb1c363c7a5fe7610997918315bca184b1d
Beryl <beryl.schragger@gmail.com>
2013-08-06 17:28:40 -0400

    Adds testing and starts functions for repo model.

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -2,4 +2,33 @@ class Repo < ActiveRecord::Base
   has_and_belongs_to_many :portfolios
   belongs_to :user
   attr_accessible :name, :html_url, :collaborators_url, :languages_url, :homepage_url
+
+  def self.updateOrCreate(info)
+    if(Repo.find_by_html_url(info[:html_url]))
+      repo = Repo.find_by_html_url(info[:html_url])
+      repo.update_attributes(info)
+      return repo
+    else
+      repo = Repo.create(info)
+      return repo
+    end
+  end
+
+  def linesOfCode()
+    return self.languages
+  end
+
+  def linesOfCodeByLanguage
+    return self.languages
+  end
+
+  def percentOfCodeByLanguage
+    return self.languages
+  end
+
+  def collaborators
+  end
+
+  def loadRepos
+  end
 end
--- spec/models/repo_spec.rb
+++ spec/models/repo_spec.rb
@@ -1,5 +1,54 @@
 require 'spec_helper'
 
-describe Repo do 
-	
+
+
+describe Repo do
+  let(:repo1) {Repo.new(name: 'coaster_project', html_url: 'https://github.com/beryls/coaster_project', collaborators_url: 'https://api.github.com/repos/beryls/coaster_project/collaborators{/collaborator}', languages_url: 'https://api.github.com/repos/beryls/coaster_project/languages', homepage_url: 'http://www.example.com')}
+
+  describe ".new" do
+    it "creates a repo object" do
+      expect(repo1).to_not eq(nil)
+    end
+  end
+
+  describe "#linesOfCode" do
+    it "returns a number" do
+      expect(repo1.linesOfCode.class).to eq ("Integer")
+    end
+  end
+
+  describe "#linesOfCodeByLanguage" do
+    it "returns a hash" do
+      expect(repo1.linesOfCodeByLanguage.class).to eq ("Hash")
+    end
+
+    it "returns numerical values for lines of code" do
+      expect(repo1.linesOfCodeByLanguage.values[0].class).to eq ("Integer")
+    end
+  end
+
+  describe "#percentOfCodeByLanguage" do
+    it "returns a hash" do
+      expect(repo1.percentOfCodeByLanguage.class).to eq ("Hash")
+    end
+
+    it "returns floats for percentage of code" do
+      expect(repo1.percentOfCodeByLanguage.values[0].class).to eq ("Float")
+    end
+  end
+
+  describe "#collaborators" do
+    it "returns an array" do
+      expect(repo1.collaborators.class).to eq ("Array")
+    end
+    it "returns the names or logins of each collaborator" do
+      expect(repo1.collaborators[0].class).to eq ("String")
+    end
+  end
+
+  describe "#updateOrCreate" do
+  end
+
+  describe "#loadRepos" do
+  end
 end
