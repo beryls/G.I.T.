9a7694b143b4ad63b469cc00996dc1f590b84655
Theodore Cleveland <tcclevela@gmail.com>
2013-08-06 14:59:46 -0400

    changes welcome index to render html

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,6 @@
 class WelcomeController < ApplicationController 
 
 	def index
-		@user = User.first
-		@portfolio = Portfolio.first
-		respond_to do |format|
-			format.json {render json: {portfolio: @portfolio, user: @user}}
-		end
 	end
 
 end	
