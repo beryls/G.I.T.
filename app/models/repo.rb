400adc28714af847a1788b0ee69ffafe332d9bb7
Beryl <beryl.schragger@gmail.com>
2013-08-05 21:46:22 -0400

    Corrects portfolio and repo models.

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,5 +1,5 @@
 class Repo < ActiveRecord::Base
   has_and_belongs_to_many :portfolios
   belongs_to :user
-  attr_accessible :name, :description
+  attr_accessible :name, :html_url, :collaborators_url, :languages_url, :homepage_url
 end
