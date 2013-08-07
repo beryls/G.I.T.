8f44d37e66640ae5905458a4ecf0531cccf5ae93
Theodore Cleveland <tcclevela@gmail.com>
2013-08-07 00:25:28 -0400

    changes name of create_user to updateOrCreate

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 	has_many :portfolios
 	attr_accessible :name, :login, :email, :avatar_url, :access_token, :repos_count
 
-	def self.create_user(info)
+	def self.updateOrCreate(info)
 		if(User.find_by_email(info[:email]))
 			user = User.find_by_email(info[:email])
 			user.update_attributes(info)
