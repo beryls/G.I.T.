75be0e3000537317a21a6ea351d79d6644c42fe8
Beryl <beryl.schragger@gmail.com>
2013-08-07 20:04:23 -0400

    Continue work on search bar.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,6 +5,9 @@ class WelcomeController < ApplicationController
 
   # make this get/search
   def search
-    @search = params[:q]
+    @search = params[:query]
+    @@access_token = current_user.access_token
+    results = RestClient.get("https://api.github.com/search/users/q=tibbon+in%3Aname+in%3Ausername&type=Users&ref=searchresults", {:params => {:access_token => @@access_token}})
+    results = JSON.parse(results)
   end
 end
