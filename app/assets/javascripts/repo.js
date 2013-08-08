48715d025f9b57adc1de094e676df6c0af98a3d6
Theodore Cleveland <tcclevela@gmail.com>
2013-08-08 14:00:53 -0400

    moves repo-rendering javascript into repo.js

--- app/assets/javascripts/repo.js
+++ app/assets/javascripts/repo.js
@@ -1,5 +1,108 @@
+function renderRepoGridCanvas(h,w) {
+
+	$('<div>').css('height', h)
+		.css('width', w)
+		.attr('id', 'repos_container')
+		.appendTo('body');
+
+	var svg = d3.select('#repos_container')
+		.append('svg')
+		.attr('height', h)
+		.attr('width', w);
+
+	return svg;
+}
+
+function renderRepoGrid(repos) {
+
+	var square = 60;
+	var h = parseInt(repos.length / 10 + 1) * square;
+	var w = 600;
+
+	svg = renderRepoGridCanvas(h,w);
+
+	svg.selectAll('rect')
+		.data(repos)
+		.enter()
+		.append('rect')
+		.attr('rx', 5)
+		.attr('x', function(d, i){
+			return (i % 10) * square;
+		})
+		.attr('y', function(d, i){
+			return (parseInt(i / 10) * square);
+		})
+		.attr('height', 55)
+		.attr('width', 55)
+		.attr('fill', function(d, i) {
+			return repoColor(d.main_language);
+		})
+		.on('mouseenter', function(d) {
+			d3.select(this)
+				.transition()
+				.duration(400)
+				.attr('rx', 100)
+				.attr('fill', function(d) {
+					return repoHover(d.main_language);
+				});
+		})
+		.on('mouseleave', function(d, i) {
+			d3.select(this)
+				.transition()
+				.duration(200)
+				.attr('rx', 5)
+				.attr('fill', function(d) {
+					return repoColor(d.main_language);
+				});
+		});
+	}
+
+function filterRepos(list) {
+	console.log(list);
+	var cleanRepos = _.reject(list, function(repo) {
+		if(!repo.main_language) {
+			return repo;
+		}
+	});
+	return cleanRepos;
+}
+
 function repoColor(lang) {
-	if(lang === "Ruby") {return '#82000e';}
-	if(lang === 'JavaScript') {return '#00a995';}
-	if(lang === 'C++') {return '#004995';}
+	if(lang === "Ruby") {return '#722124';}
+	if(lang === 'JavaScript') {return '#3aa0a0';}
+	if(lang === 'C++') {return '#624588';}
+	else {
+		return generateColor(lang);
+	}
+}
+
+function repoHover(lang) {
+	if(lang === "Ruby") {return '#ca2830';}
+	if(lang === 'JavaScript') {return '#47c4c4';}
+	if(lang === 'C++') {return '#8e65c5';}
+	else {
+		return generateHover(lang);
+	}
+}
+
+function generateColor(lang) {
+	if (lang.length < 6) {
+		while(lang.length < 6) {
+			lang += 'ax';
+		}
+	}
+	return 'rgb(' + Math.abs(lang[0].charCodeAt(0) - lang[1].charCodeAt(0)) * 3 +
+					"," + Math.abs(lang[2].charCodeAt(0) - lang[3].charCodeAt(0)) * 4 +
+					"," + Math.abs(lang[4].charCodeAt(0) - lang[5].charCodeAt(0)) * 5+ ")";
+}
+
+function generateHover(lang) {
+	if (lang.length < 6) {
+		while(lang.length < 6) {
+			lang += 'ax';
+		}
+	}
+	return 'rgb(' + Math.abs(lang[0].charCodeAt(0) - lang[1].charCodeAt(0)) * 4 +
+					"," + Math.abs(lang[2].charCodeAt(0) - lang[3].charCodeAt(0)) * 5 +
+					"," + Math.abs(lang[4].charCodeAt(0) - lang[5].charCodeAt(0)) * 6+ ")";
 }
--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -2,57 +2,17 @@ function renderUserProfile(user_json, repos_json) {
 
 	var user = [],repos;
 	user[0] = $.parseJSON(user_json);
-	repos = $.parseJSON(repos_json);
-	console.log(repos);
+	repos = filterRepos($.parseJSON(repos_json));
 
-	var square = 60
+	var square = 60;
 
-	var h = parseInt(repos.length / 10 + 1) * square;
-	var w = 600;
+		$('<div>')
+			.attr('id', 'profile_box')
+			.appendTo('body');
 
-	$('<div>').css('height', h)
-		.css('width', w)
-		.attr('id', 'canvas_container')
-		.appendTo('body');
+		$
 
-	var svg = d3.select('#canvas_container')
-		.append('svg')
-		.attr('height', h)
-		.attr('width', w);
-
-	svg.selectAll('rect')
-		.data(repos)
-		.enter()
-		.append('rect')
-		.attr('rx', 5)
-		.attr('x', function(d, i){
-			return (i % 10) * square;
-		})
-		.attr('y', function(d, i){
-			return (parseInt(i / 10) * square);
-		})
-		.attr('height', 55)
-		.attr('width', 55)
-		.attr('fill', function(d, i) {
-			console.log(d.main_language)
-			return repoColor(d.main_language);
-		})
-		.on('mouseenter', function(d) {
-			d3.select(this)
-				.transition()
-				.duration(200)
-				.attr('fill', 'blue')
-				.attr('height', square)
-				.attr('width', square);
-		})
-		.on('mouseleave', function(d, i) {
-			d3.select(this)
-				.transition()
-				.duration(200)
-				.attr('fill', 'black')
-				.attr('height', 55)
-				.attr('width', 55);
-		});
+	renderRepoGrid(repos);
 
 }
 
