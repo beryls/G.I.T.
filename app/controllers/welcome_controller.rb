c698e1a9bb1239d7d5043ce5dd8b054c32395bd5
Beryl <beryl.schragger@gmail.com>
2013-08-08 14:10:36 -0400

    Allows for search of users from GitHub API - results are currently not formatted.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,16 +4,9 @@ class WelcomeController < ApplicationController
     if current_user
       @@access_token = current_user.access_token
     end
+    @search = params[:q]
+    @results = RestClient.get("https://api.github.com/legacy/user/search/#{@search}+in%3Aname+in%3Alogin&type=Users&ref=searchresults", {:params => {:access_token => @@access_token}})
+    @results = JSON.parse(@results)
 	end
 
-  # make this get/search
-  def search
-    @search = params[:q]
-    @@access_token = current_user.access_token
-    @results = RestClient.get("https://api.github.com/search/users?q=tibbon+in%3Aname+in%3Alogin&type=Users&ref=searchresults", {:params => {:access_token => @@access_token}})
-    @results = JSON.parse(results)
-    # https://api.github.com/search/users?q=tom
-    # ?q=tibbon+in%3Aname+in%3Alogin&type=Users&ref=searchresults
-    # :q => @search,
-  end
 end
