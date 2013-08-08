e1e688b5298fb7ffb17adfec9f755430d33418ed
Theodore Cleveland <tcclevela@gmail.com>
2013-08-08 15:46:53 -0400

    adds backgrounds to profile page

--- app/assets/javascripts/repo.js
+++ app/assets/javascripts/repo.js
@@ -16,8 +16,8 @@ function renderRepoGridCanvas(h,w) {
 function renderRepoGrid(repos) {
 
 	var square = 60;
-	var h = parseInt(repos.length / 10 + 1) * square;
-	var w = 600;
+	var h = parseInt(repos.length / 15 + 1) * square;
+	var w = 15 * 60;
 
 	svg = renderRepoGridCanvas(h,w);
 
@@ -27,42 +27,48 @@ function renderRepoGrid(repos) {
 		.append('rect')
 		.attr('rx', 3)
 		.attr('x', function(d, i){
-			return (i % 10) * square;
+			return (i % 15) * square;
 		})
 		.attr('y', function(d, i){
-			return (parseInt(i / 10) * square);
+			return parseInt(i / 15) * square;
 		})
 		.attr('height', 0)
 		.attr('width', 55)
-		.attr('fill', 'white')
-		.on('mouseenter', function(d) {
-			d3.select(this)
-				.transition()
-				.duration(400)
-				.attr('rx', 20)
-				.attr('fill', function(d) {
-					return repoHover(d.main_language);
-				});
-		})
-		.on('mouseleave', function(d, i) {
-			d3.select(this)
-				.transition()
-				.duration(200)
-				.attr('rx', 3)
-				.attr('fill', function(d) {
-					return repoColor(d.main_language);
-				});
+		.attr('opacity', 0)
+		.attr('fill', function(d, i) {
+			return repoColor(d.main_language);
 		})
 		.transition()
 		.duration(1250)
 		.attr('height', 55)
-		.attr('fill', function(d, i) {
-			return repoColor(d.main_language);
-		})
+		.attr('opacity', 1)
+		.each('end', function() {
+			d3.select(this).on('mouseenter', function(d) {
+				d3.select(this)
+					.transition()
+					.duration(400)
+					
+					.attr('fill', function(d) {
+						return repoHover(d.main_language);
+					})
+					.transition()
+					.duration(600)
+					.attr('rx', 25);
+			})
+			.on('mouseleave', function(d, i) {
+				d3.select(this)
+					.transition()
+					.duration(200)
+					.attr('rx', 3)
+					.attr('fill', function(d) {
+						return repoColor(d.main_language);
+					});
+			});
+		});
+
 	}
 
 function filterRepos(list) {
-	console.log(list);
 	var cleanRepos = _.reject(list, function(repo) {
 		if(!repo.main_language) {
 			return repo;
