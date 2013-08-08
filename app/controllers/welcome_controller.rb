19aca1666c13514132e659cc6a067d79783d249d
Beryl <beryl.schragger@gmail.com>
2013-08-08 14:18:37 -0400

    Allows for URI encoding of searches.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,9 @@ class WelcomeController < ApplicationController
     if current_user
       @@access_token = current_user.access_token
     end
-    @search = params[:q]
+    if params[:q]
+      @search = URI.encode(params[:q])
+    end
     @results = RestClient.get("https://api.github.com/legacy/user/search/#{@search}+in%3Aname+in%3Alogin&type=Users&ref=searchresults", {:params => {:access_token => @@access_token}})
     @results = JSON.parse(@results)
 	end
